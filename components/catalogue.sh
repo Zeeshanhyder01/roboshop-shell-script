428db6e2b70cf3caf4848eda81ff966dd216b6d5
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-17 13:56:00 +0530

    catalogue setup

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -6,7 +6,7 @@ yum install nodejs -y
 useradd roboshop
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
-rm-rf catalogue
+rm -rf catalogue
 unzip /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
