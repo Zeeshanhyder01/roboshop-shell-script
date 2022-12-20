bafb4fa1d5edcea5fe87df95544cff343fe6ee0b
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-20 21:04:58 +0530

    mongo

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -17,6 +17,6 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 cd /tmp
 unzip mongodb.zip
 cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+mongo <catalogue.js
+mongo <users.js
 
