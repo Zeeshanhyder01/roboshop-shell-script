7248806743a07659583204e1c8fc94fe5f690eeb
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-20 20:52:15 +0530

    catalogue-installation

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+cd /home/roboshop
+rm -rf catalogue
+unzip /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+npm install
+#Update SystemD file with correct IP addresses   #
+# Update `MONGO_DNSNAME` with MongoDB Server IP
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /etc/systemd/system/catalogue.service
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
 
