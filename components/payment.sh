eaaba5bd2b9186f72cbccdfd70f48d0019dc7ce9
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-26 13:39:35 +0530

    payment

--- components/payment.sh
+++ components/payment.sh
@@ -8,8 +8,8 @@ unzip /tmp/payment.zip
 mv payment-main payment
 cd /home/roboshop/payment
 pip3 install -r requirements.txt
-USER_ID = $(id -u roboshop)
-GROUP_ID = $(id -g roboshop)
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
 
 sed -i -e "/^uid/c uid =${USER_ID}" -e "/^gid/c gid =${GROUP_ID}" /home/roboshop/payment/payment.ini
 
