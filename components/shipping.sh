ac304c804207879aaf8b4c9bf9050329f3183c27
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-26 12:40:25 +0530

    shipping

--- components/shipping.sh
+++ components/shipping.sh
@@ -1,1 +1,21 @@
-echo install shipping  component
+#!/usr/bin/env bash
+
+yum install maven -y
+useradd roboshop
+cd /home/roboshop
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+unzip /tmp/shipping.zip
+mv shipping-main shipping
+cd shipping
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+sed -i -e "s/CARTENDPOINT/cart.roboshop.internal/" /home/roboshop/shipping/systemd.service
+sed -i -e "s/DBHOST/mysql.roboshop.internal/" /home/roboshop/shipping/systemd.service
+#1. Update SystemD Service file
+# Update `CARTENDPOINT` with Cart Server IP.
+#Update `DBHOST` with MySQL Server IP
+
+mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+systemctl start shipping
+systemctl enable shipping
