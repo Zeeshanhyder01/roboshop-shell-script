0ab1f73cab8092836981e4f6b5112e678ab16879
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-29 17:48:44 +0530

    keeping code DRY

--- components/common.sh
+++ components/common.sh
@@ -4,7 +4,7 @@ CHECK_ROOT()
   USER_ID=$(id -u)
   if [ $USER_ID -ne 0 ]; then
     echo You are Non root user
-    echo You can Run this script using SUDO
+    echo You can Run this script using sudo
     exit 1
 
   fi
