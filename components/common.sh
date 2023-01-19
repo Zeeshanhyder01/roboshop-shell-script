fbcf4f969ccda0dec15b35474dfca1f6700c797f
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-01-19 19:53:51 +0530

    rerun

--- components/common.sh
+++ components/common.sh
@@ -3,10 +3,9 @@ CHECK_ROOT()
 {
   USER_ID=$(id -u)
   if [ $USER_ID -ne 0 ]; then
-    echo You are Non root user
-    echo You can Run this script using sudo
+    echo -e "\e[31m You should be running  this script as root user or using sudo \e[0m"
     exit 1
 
   fi
 }
-CHECK_ROOT
+
