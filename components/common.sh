38923053ad9a80acca1c0443ea6d7f0aaf51ede5
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-01-20 13:03:57 +0530

    automation

--- components/common.sh
+++ components/common.sh
@@ -9,12 +9,15 @@ CHECK_ROOT() {
 }
 
 CHECK_STAT() {
-  if [ $1 -ne 0 ]; then
-    echo -e "\e[31m FAILED \e[0m"
-    exit 2
-    else
-      echo -e "\e[32m SUCCESS \e[0m"
-  fi
+echo "-------------------------" >>${LOG}
+if [ $1 -ne 0 ]; then
+  echo -e "\e[31mFAILED\e[0m"
+  echo -e "\n Check log file - ${LOG} for errors\n"
+  exit 2
+else
+  echo -e "\e[32mSUCCESS\e[0m"
+fi
 }
+
 LOG=/tmp/roboshop.log
 rm -f $LOG
