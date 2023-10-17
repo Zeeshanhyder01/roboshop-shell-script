a508387687b2b0c6e35f8edbbf5163eb944d2735
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-17 19:07:51 +0530

    roboshop-project-shellscripting

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -4,11 +4,9 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 yum install -y mongodb-org
 systemctl enable mongod
 systemctl start mongod
-sed -e -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
-#1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
-#Config file: `/etc/mongod.conf`
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
 systemctl restart mongod
- curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
 unzip mongodb.zip
 cd mongodb-main
