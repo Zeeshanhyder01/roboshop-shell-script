f3507177ab113d010bc6cbc611428dd4bf8b4b6e
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-17 19:20:42 +0530

    roboshop-project-shellscripting

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-
+#mongo
 yum install -y mongodb-org
 systemctl enable mongod
 systemctl start mongod
