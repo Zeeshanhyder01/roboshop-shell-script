6951a8d9b7f172b57900f15780d5fc592a98ac94
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-18 17:06:05 +0530

    roboshop-project-shellscripting

--- components/user.sh
+++ components/user.sh
@@ -4,6 +4,7 @@ yum install nodejs -y
 useradd roboshop
 $ curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
 $ cd /home/roboshop
+rm -rf user
 $ unzip /tmp/user.zip
 $ mv user-main user
 $ cd /home/roboshop/user
