f601e524f447fae4b23c5c1f9d55019c78a2d732
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-18 15:52:52 +0530

    roboshop-project-shellscripting

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -10,7 +10,7 @@ unzip /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
-sed -e -i's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl start catalogue
