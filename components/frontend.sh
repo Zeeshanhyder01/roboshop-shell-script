8e2e7e975c0bba3b45d70373ebe7b2b0b4a7b8d5
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-19 20:17:12 +0530

    shell script

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
 USER_ID=$(id -u)
-if [ $USER_ID -eq 0 ] ; then
+if [ $USER_ID -ne 0 ] ; then
 echo you are non root user
 echo you can run this scrpit as a root user or use sudo
 exit 1
 fi
+
 echo Installing Nginx
 yum install nginx -y
 systemctl enable nginx
