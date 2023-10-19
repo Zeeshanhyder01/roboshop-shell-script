9acfcd19d84d0c4c27fee43484064714f58a910b
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-19 20:12:57 +0530

    shell script-exit status|command

--- components/cart.sh
+++ components/cart.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ] ; then
+echo you are non root user
+echo you can run this scrpit as a root user or use sudo
+exit 1
+fi
+
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
 yum install nodejs -y
 useradd roboshop
