f79e65f39d620d0a0736c13ee4068c87f79b9f1b
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-19 20:15:54 +0530

    shell script

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ] ; then
+if [ $USER_ID -eq 0 ] ; then
 echo you are non root user
 echo you can run this scrpit as a root user or use sudo
 exit 1
