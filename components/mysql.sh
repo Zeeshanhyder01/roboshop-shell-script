08ee1758837544b8a37f72efa0271bb5602d7d26
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-19 12:00:17 +0530

    roboshop-project-shellscripting

--- components/mysql.sh
+++ components/mysql.sh
@@ -5,7 +5,7 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" > /tmp/mysql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >/tmp/mysql
 mysql --connect-expired-password -uRoot -p"${MYSQL_DEFAULT_PASSWORD}" < /tmp/mysql
 
 #1. Now a default root password will be generated and given in the log file.
