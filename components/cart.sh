0948c4eb1d7fe62aa296c23bd602095396c43797
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-20 15:50:03 +0530

    updating-roboshop-shell-project

--- components/cart.sh
+++ components/cart.sh
@@ -2,48 +2,48 @@
 source components/common.sh
 CHECK_ROOT
 
-echo -e "\e[31m SETTING UP THE NODEJS YUM REPO IS \e[0"
+echo -e "\e[31m SETTING UP THE NODEJS YUM REPO IS \e[0m"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>${LOG}
 CHECK_STAT $?
 
-echo -e "\e[31m Installing NODEJS YUM REPO  \e[0"
+echo -e "\e[31m Installing NODEJS YUM REPO  \e[0m"
 yum install nodejs -y  &>>${LOG}
 CHECK_STAT $?
 
-echo -e "\e[33m creating an Application user \e[0"
+echo -e "\e[33m creating an Application user \e[0m"
 useradd roboshop &>>${LOG}
 CHECK_STAT $?
 
-echo -e "\e[34m Downloading  cart content \e[0"
+echo -e "\e[34m Downloading  cart content \e[0m"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  &>>${LOG}
 CHECK_STAT $?
 
 cd /home/roboshop
-echo -e "\e[31m Remove OLD content \e[0"
+echo -e "\e[31m Remove OLD content \e[0m"
 rm -rf cart &>>${LOG}
 CHECK_STAT $?
 
-echo -e "\e[33m unzipping / extract cart content\e[0"
+echo -e "\e[33m unzipping / extract cart content\e[0m"
 unzip /tmp/cart.zip &>>${LOG}
 CHECK_STAT $?
 
 mv cart-main cart
 cd cart
-echo -e "\e[31m Install NODEJS Dependencies for cart Component\e[0"
+echo -e "\e[31m Install NODEJS Dependencies for cart Component\e[0m"
 npm install &>>${LOG}
 CHECK_STAT $?
 
-echo -e "\e[31m Update systemd Configuration \e[o"
+echo -e "\e[31m Update systemd Configuration \e[0m"
 sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/cart.service &>>${LOG}
 CHECK_STAT $?
 
-echo -e "\e[31m SetUp systemd Configuration \e[o"
+echo -e "\e[31m SetUp systemd Configuration \e[0m"
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service  &>>${LOG}
 CHECK_STAT $?
 
 systemctl daemon-reload
 systemctl enable cart
 
-echo -e "\e[31m start cart service \e[o"
+echo -e "\e[31m start cart service \e[0"
 systemctl restart cart  &>>${LOG}
 
