67a176aad6b224f0b3c5143d8721b60be80faa4f
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-20 14:51:37 +0530

    updating-roboshop-shell-project

--- components/common.sh
+++ components/common.sh
@@ -3,7 +3,7 @@
 CHECK_ROOT(){
   USER_ID=$(id -u)
   if [ $USER_ID -ne 0 ] ; then
-  echo -e"\e[31m you should run this script as a root user or you can use sudo\e[0m"
+  echo -e "\e[31m you should run this script as a root user or you can use sudo\e[0"
   exit 1
   fi
 }
