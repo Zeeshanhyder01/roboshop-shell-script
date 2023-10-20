321877bb248d04320081316df37c8d4fbd70ed4b
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-20 15:42:23 +0530

    updating-roboshop-shell-project

--- components/cart.sh
+++ components/cart.sh
@@ -1,21 +1,48 @@
 #!/usr/bin/env bash
 source components/common.sh
 CHECK_ROOT
+
 echo -e "\e[31m SETTING UP THE NODEJS YUM REPO IS \e[0"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>${LOG}
 CHECK_STAT $?
-yum install nodejs -y
-useradd roboshop
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+
+echo -e "\e[31m Installing NODEJS YUM REPO  \e[0"
+yum install nodejs -y  &>>${LOG}
+CHECK_STAT $?
+
+echo -e "\e[33m creating an Application user \e[0"
+useradd roboshop &>>${LOG}
+CHECK_STAT $?
+
+echo -e "\e[34m Downloading  cart content \e[0"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  &>>${LOG}
+CHECK_STAT $?
+
 cd /home/roboshop
-rm -rf cart
-unzip /tmp/cart.zip
+echo -e "\e[31m Remove OLD content \e[0"
+rm -rf cart &>>${LOG}
+CHECK_STAT $?
+
+echo -e "\e[33m unzipping / extract cart content\e[0"
+unzip /tmp/cart.zip &>>${LOG}
+CHECK_STAT $?
+
 mv cart-main cart
 cd cart
-npm install
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' /etc/systemd/system/cart.service
-sed -i -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/cart.service
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+echo -e "\e[31m Install NODEJS Dependencies for cart Component\e[0"
+npm install &>>${LOG}
+CHECK_STAT $?
+
+echo -e "\e[31m Update systemd Configuration \e[o"
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/cart.service &>>${LOG}
+CHECK_STAT $?
+
+echo -e "\e[31m SetUp systemd Configuration \e[o"
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service  &>>${LOG}
+CHECK_STAT $?
+
 systemctl daemon-reload
-systemctl start cart
 systemctl enable cart
+
+echo -e "\e[31m start cart service \e[o"
+systemctl restart cart  &>>${LOG}
