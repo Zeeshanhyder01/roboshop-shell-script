7b14439fb806d6f5a7f38312319cc5b2ac5f9ef5
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-23 14:52:00 +0530

    updating-roboshop-shell-project

--- components/cart.sh
+++ components/cart.sh
@@ -2,47 +2,50 @@
 source components/common.sh
 CHECK_ROOT
 
-echo -e "\e[31m SETTING UP THE NODEJS YUM REPO IS \e[0m"
+print "SETTING UP THE NODEJS YUM REPO IS"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>${LOG}
 CHECK_STAT $?
 
-echo Installing NODEJS YUM REPO
+print "Installing NODEJS YUM REPO"
 yum install nodejs -y  &>>${LOG}
 CHECK_STAT $?
 
-echo  creating an Application user
-useradd roboshop &>>${LOG}
+print "creating an Application user"
+id roboshop &>>${LOG}
+if [ $? -ne 0 ] ; then
+  useradd roboshop &>>${LOG}
+  fi
 CHECK_STAT $?
 
-echo  Downloading  cart content
+print  "Downloading  cart content"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  &>>${LOG}
 CHECK_STAT $?
 
 cd /home/roboshop
-echo Remove OLD content
+print "Remove OLD content"
 rm -rf cart &>>${LOG}
 CHECK_STAT $?
 
-echo  unzipping / extract cart content
+print  "unzipping / extract cart content"
 unzip /tmp/cart.zip &>>${LOG}
 CHECK_STAT $?
 
 mv cart-main cart
 cd cart
-echo  Install NODEJS Dependencies for cart Component
+print  "Install NODEJS Dependencies for cart Component"
 npm install &>>${LOG}
 CHECK_STAT $?
 
-echo  Update systemd Configuration
+print  "Update systemd Configuration"
 sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/cart.service &>>${LOG}
 CHECK_STAT $?
 
-echo SetUp systemd Configuration
+print "SetUp systemd Configuration"
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service  &>>${LOG}
 CHECK_STAT $?
 
 systemctl daemon-reload
 systemctl enable cart
-echo start cart service
+print "start cart service"
 systemctl restart cart  &>>${LOG}
 
