d574c5adc4d836c6be32aa0aadc9161b0e341b06
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-23 17:47:09 +0530

    updating-roboshop-shell-project

--- components/common.sh
+++ components/common.sh
@@ -26,54 +26,54 @@ PRINT() {
   echo "----------$1---------">>${LOG}
   echo "$1"
 }
-#
-#NODEJS() {
-#CHECK_ROOT
-#
-#printf "SETTING UP THE NODEJS YUM REPO IS"
-#curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>${LOG}
-#CHECK_STAT $?
-#
-#printf "Installing NODEJS YUM REPO"
-#yum install nodejs -y  &>>${LOG}
-#CHECK_STAT $?
-#
-#printf "creating an Application user"
-#id roboshop &>>${LOG}
-#if [ $? -ne 0 ] ; then
-#  useradd roboshop &>>${LOG}
-#  fi
-#CHECK_STAT $?
-#
-#printf "Downloading  ${COMPONENT} content"
-#curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip"  &>>${LOG}
-#CHECK_STAT $?
-#
-#cd /home/roboshop
-#printf "Remove OLD content"
-#rm -rf ${COMPONENT} &>>${LOG}
-#CHECK_STAT $?
-#
-#printf "unzipping / extract cart content"
-#unzip /tmp/${COMPONENT}.zip &>>${LOG}
-#CHECK_STAT $?
-#
-#mv ${COMPONENT}-main ${COMPONENT}
-#cd cart
-#printf "Install NODEJS Dependencies for cart Component"
-#npm install &>>${LOG}
-#CHECK_STAT $?
-#
-#printf "Update systemd Configuration"
-#sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/${COMPONENT}.service &>>${LOG}
-#CHECK_STAT $?
-#
-#printf "SetUp systemd Configuration"
-#mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG}
-#CHECK_STAT $?
-#
-#systemctl daemon-reload
-#systemctl enable ${COMPONENT} &>>${LOG}
-#printf "start ${COMPONENT} service"
-#systemctl restart ${COMPONENT}  &>>${LOG}
-#}
+
+NODEJS() {
+  CHECK_ROOT
+  PRINT "SETTING UP THE NODEJS YUM REPO IS"
+  curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Installing NODEJS YUM REPO"
+  yum install nodejs -y  &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "creating an Application user"
+  id roboshop &>>${LOG}
+   if [ $? -ne 0 ] ; then
+    useradd roboshop &>>${LOG}
+    fi
+  CHECK_STAT $?
+
+  PRINT "Downloading cart content"
+  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  &>>${LOG}
+  CHECK_STAT $?
+
+  cd /home/roboshop
+  PRINT "Remove OLD content"
+  rm -rf cart &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "unzipping / extract cart content"
+  unzip /tmp/cart.zip &>>${LOG}
+  CHECK_STAT $?
+
+  mv cart-main cart
+  cd cart
+  PRINT "Install NODEJS Dependencies for cart Component"
+  npm install &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Update systemd Configuration"
+  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/cart.service &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "SetUp systemd Configuration"
+  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service  &>>${LOG}
+  CHECK_STAT $?
+
+  systemctl daemon-reload
+  systemctl enable cart &>>${LOG}
+  PRINT "start cart service"
+  systemctl restart cart  &>>${LOG}
+  CHECK_STAT $?
+}
