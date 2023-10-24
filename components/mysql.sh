7861f472b0c5c3649d1c0834110e001f23b3789e
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-24 12:08:12 +0530

    updating-roboshop-shell-project

--- components/mysql.sh
+++ components/mysql.sh
@@ -19,8 +19,13 @@ CHECK_STAT $?
 
 PRINT "RESET ROOT PASSWORD "
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}" &>>${LOG}
-CHECK_STAT $?
+
+echo show databases | mysql -uroot -p"${MYSQL_PASSWORD}" &>>${LOG}
+if [ $? -ne 0 ]; then
+  PRINT "RESET Root Password"
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}" &>>${LOG}
+  CHECK_STAT $?
+fi
 exit 2
 
 echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
