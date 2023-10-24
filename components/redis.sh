78e0bfd8b1b6ab6159a842a6968f6e5df0f56df2
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-24 12:22:10 +0530

    updating-roboshop-shell-project

--- components/redis.sh
+++ components/redis.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 source components/common.sh
 CHECK_ROOT
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-yum install redis-6.2.13 -y
-sed -i -e's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
-systemctl enable redis
-systemctl start redis
+
+PRINT " CONFIGURE YUM REPO FOR REDIS:"
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+CHECK_STAT $?
+
+PRINT " INSTALLING REDIS "
+yum install redis-6.2.13 -y  &>>${LOG}
+CHECK_STAT $?
+
+PRINT "UPDATE REDIS CONFIGURATION "
+sed -i -e's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+CHECK_STAT $?
+
+PRINT "START REDIS SERVICE"
+systemctl enable redis &>>${LOG}  && systemctl start redis &>>${LOG}
+CHECK_STAT $?
