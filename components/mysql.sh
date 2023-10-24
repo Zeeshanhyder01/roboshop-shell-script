d19cb0e3263f9d70884a5c61dbfdbe6b1e1c9c2d
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-24 11:43:54 +0530

    updating-roboshop-shell-project

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 source components/common.sh
 CHECK_ROOT
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
+
+PRINT "CONFIGURE YUM REPOS "
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+CHECK_STAT $?
+
+PRINT " INSTALL MY SQL"
+yum install mysql-community-server -y  &>>${LOG}
+systemctl enable mysqld  &>>${LOG} && systemctl start mysqld  &>>${LOG}
+CHECK_STAT $?
+
+PRINT "RESET ROOT PASSWORD "
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >/tmp/mysql
-mysql --connect-expired-password -uRoot -p"${MYSQL_DEFAULT_PASSWORD}" < /tmp/mysql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >/tmp/mysql | mysql --connect-expired-password -uRoot -p"${MYSQL_DEFAULT_PASSWORD}" < /tmp/mysql
+CHECK_STAT $?
+exit
+
 echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
 
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
