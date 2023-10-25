76004160b150e02783350ab4b10558a3427da251
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-25 17:35:38 +0530

    Loops

--- components/common.sh
+++ components/common.sh
@@ -99,8 +99,9 @@ NGINX(){
     CHECK_STAT $?
     PRINT " ORGANISE ${COMPONENT}  CONTENT"
     mv ${COMPONENT}-main/* . && mv static/* . && rm -rf ${COMPONENT}-main README.md && mv localhost.conf /etc/nginx/default.d/roboshop.conf
-    PRINT " UPDATE ${COMPONENT} CONFIGURATION"
-    sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+    for backend in catalogue cart user shipping payment
+    PRINT " UPDATE CONFIGURATION for -$backend"
+#    sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
     CHECK_STAT $?
     PRINT " START NGINX SERVICE "
     systemctl enable nginx &>>${LOG} && systemctl restart nginx  &>>${LOG}
